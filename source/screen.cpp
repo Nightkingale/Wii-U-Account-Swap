196ed99087caa11adc6759c81ffc30d9d412cdde
Nightkingale <63483138+Nightkingale@users.noreply.github.com>
2024-05-06 19:09:17 -0600

    Add messages at the bottom of some screens

--- source/screen.cpp
+++ source/screen.cpp
@@ -77,7 +77,7 @@ void draw_unlink_menu() {
     draw_background(16, 16, 16, 255);
     draw_screen_bars();
 
-    draw_text("Unlinking: Please read the following and confirm!", 64, 120, 50);
+    draw_text("Before unlinking, please read the following notice!", 64, 120, 50);
 
     draw_text("This will unlink your Network ID from this user.", 64, 230, 50);
     draw_text("You can reattach this account to any user on this Wii U,", 64, 290, 50);
@@ -85,6 +85,8 @@ void draw_unlink_menu() {
 
     draw_text("However, this unlink will not take place on the server.", 64, 460, 50);
     draw_text("You won't be able to use this account on any other Wii U.", 64, 520, 50);
+
+    draw_text("You must confirm to continue the process.", 64, 840, 50);
     
     draw_confirm_button();
 
@@ -96,7 +98,7 @@ void draw_backup_menu() {
     draw_background(16, 16, 16, 255);
     draw_screen_bars();
 
-    draw_text("Backup: Please read the following and confirm!", 64, 120, 50);
+    draw_text("Before backing up, please read the following notice!", 64, 120, 50);
 
     draw_text("This will backup your current account.dat file.", 64, 230, 50);
     draw_text("The account.dat may contain sensitive personal", 64, 340, 50);
@@ -104,6 +106,8 @@ void draw_backup_menu() {
     draw_text("cached password (if you have chosen to save it).", 64, 460, 50);
 
     draw_text("Please do not share these backups with anyone else!", 64, 570, 50);
+
+    draw_text("You must confirm to continue the process.", 64, 840, 50);
 
     draw_confirm_button();
 
@@ -115,12 +119,14 @@ void draw_overwrite_menu(const char* backup_path) {
     draw_background(10, 10, 60, 255); // Blue background.
     draw_screen_bars();
 
-    draw_text("Backup: Please read the following and confirm!", 64, 120, 50);
+    draw_text("Before backing up, please read the following notice!", 64, 120, 50);
 
     draw_text("The backup file already exists!", 64, 230, 50);
     draw_text(backup_path, 64, 290, 50);
 
     draw_text("Would you like to overwrite it?", 64, 400, 50);
+
+    draw_text("You must confirm to continue the process.", 64, 840, 50);
 
     draw_confirm_button();
 
@@ -136,6 +142,8 @@ void draw_error_menu(const char* error_message) {
 
     draw_text(error_message, 64, 230, 50);
     draw_text("You will return to the main menu.", 64, 290, 50);
+
+    draw_text("If you believe this to be a bug, please report it.", 64, 840, 50);
 
     SDL_RenderPresent(renderer);
     SDL_Delay(5000);
@@ -163,7 +171,7 @@ void draw_success_menu(const char* type, bool inkay_configured = false) {
         draw_text("Your console will restart in 5 seconds...", 64, 290, 50);
 
         if (inkay_configured) {
-            draw_text("Inkay was also configured automatically!", 64, 400, 50);
+            draw_text("Please note that Inkay was configured automatically.", 64, 840, 50);
         }
     }
 
